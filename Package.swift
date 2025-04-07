2bd50c2c2b66ab25ad51e0e8feac17b8b48b0d33
Leo Mehlig <leo@leomehlig.com>
2025-04-07 21:04:36 +0200

    update tools version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(name: "WheelPicker",
                       platforms: [
-                          .iOS(.v14)
+                        .iOS(.v16), .watchOS(.v9), .macCatalyst(.v16), .macOS(.v13)
                       ],
                       products: [
                           .library(name: "WheelPicker",
